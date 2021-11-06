8e67ebeedd8b082954f2ecedcdf69d2db49040dd
sizovk <sizovki@yandex.ru>
2021-11-06 22:02:09 +0300

    added anno pies

---
+++ src/ChIPpeakAnno.R
@@ -0,0 +1,42 @@
+
+source('lib.R')
+
+###
+
+# https://bioconductor.org/packages/release/bioc/vignettes/ChIPpeakAnno/inst/doc/quickStart.html
+# BiocManager::install("ChIPpeakAnno")
+# BiocManager::install("org.Hs.eg.db")
+# BiocManager::install("org.Mm.eg.db")
+
+library(ChIPpeakAnno)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(org.Hs.eg.db)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+#library(org.Mm.eg.db)
+
+###
+
+
+peaks <- toGRanges(paste0(DATA_DIR, 'H2AFZ.parathyroid_adenoma.intersect_with_DeepZ.bed'), format="BED")
+peaks[1:2]
+
+annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
+annoData[1:2]
+
+
+anno <- annotatePeakInBatch(peaks, AnnotationData=annoData, 
+                            output="overlapping", 
+                            FeatureLocForDistance="TSS",
+                            bindingRegion=c(-2000, 300))
+data.frame(anno) %>% head()
+
+anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
+data.frame(anno) %>% head()
+
+anno_df <- data.frame(anno)
+write.table(anno_df, file=paste0(DATA_DIR, 'H2AFZ.parathyroid_adenoma.intersect_with_DeepZ.genes.txt'),
+            col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+uniq_genes_df <- unique(anno_df['symbol'])
+write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H2AFZ.parathyroid_adenoma.intersect_with_DeepZ.genes_uniq.txt'),
+            col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
---
+++ src/lib.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+###
+
+DATA_DIR <- '../data/'
+OUT_DIR <- '../images/'
